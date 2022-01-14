f6243aa7c97d594f646113258bc7269da77ec0f0
millernerd <millernerd@gmail.com>
2022-01-13 19:49:06 -0700

    echo time input

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", (req, res, next) => {
+  res.json({echo: req.params.date})
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
